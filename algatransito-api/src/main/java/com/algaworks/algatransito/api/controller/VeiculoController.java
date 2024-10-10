44a8ef63bda5e7565d18daa83fd68fb4657b3e1a
BrunoZambuze <zambuzesilva@hotmail.com>
2024-10-10 16:41:11 -0300

    Criação do DTO do veiculo e refatoração do controller. Adicionado a dependência do ModelMapper e suas configurações. Adicionado a lógica para resolver o erro dos nomes dos atributos do Domain e Representation estarem diferentes

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
@@ -1,16 +1,18 @@
 package com.algaworks.algatransito.api.controller;
 
-import com.algaworks.algatransito.domain.exception.RegraDeNegocioException;
+import com.algaworks.algatransito.api.representationmodel.VeiculoRepresentationModel;
 import com.algaworks.algatransito.domain.model.Veiculo;
 import com.algaworks.algatransito.domain.repository.VeiculoRepository;
 import com.algaworks.algatransito.domain.service.RegistroVeiculoService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @RequestMapping("/veiculos") //Irá informar que todos os endpoints desse controlodar começará com /veiculos
@@ -19,15 +21,28 @@ public class VeiculoController {
 
     private final VeiculoRepository veiculoRepository;
     private final RegistroVeiculoService registroVeiculoService;
+    private final ModelMapper modelMapper; //<--- Responsável por mapear o domain model em um representation model (isso facilita para não precisar
+                                                                                                          //ficar repetindo códigos a cada método)
+    /*
+    Explicação: Para utilizar o ModelMapper precisamos adicionar uma dependência no nosso maven no .xml, porém o Model Mapper NÃO é um componente
+    do Spring, ou seja, ele não conseguirá injetar uma nova instância através do Lombok. Para resolver esse problema, criamos uma pasta "common" ou
+    "config", e dentro dela terá uma classe "ModelMaperConfig". Sempre que precisar de uma instância de ModelMapper, o Spring chamará o método
+    modelMapper() para fornecer a instância.
+     */
 
     @GetMapping
-    public List<Veiculo> listar(){
-        return veiculoRepository.findAll();
+    public List<VeiculoRepresentationModel> listar(){
+        return veiculoRepository.findAll()
+                                .stream()
+                                .map(veiculo -> modelMapper.map(veiculo, VeiculoRepresentationModel.class))
+                                .collect(Collectors.toList());
     }
 
     @GetMapping("/{veiculoId}")
-    public ResponseEntity<Veiculo> buscar(@PathVariable Long veiculoId){
-        return veiculoRepository.findById(veiculoId)
+    public ResponseEntity<VeiculoRepresentationModel> buscar(@PathVariable Long veiculoId){
+        return veiculoRepository.findById(veiculoId)      //vamos estar mapeando o veiculo para uma nova instancia de VeiculoRepresentationModel
+                                                         //já com todas as propriedades preenchidas
+                                .map(veiculo -> modelMapper.map(veiculo, VeiculoRepresentationModel.class))
                                 .map(v -> ResponseEntity.ok(v))
                                 .orElse(ResponseEntity.notFound().build());
     }
---
+++ algatransito-api/src/main/java/com/algaworks/algatransito/common/ModelMapperConfig.java
@@ -0,0 +1,27 @@
+package com.algaworks.algatransito.common;
+
+import com.algaworks.algatransito.api.representationmodel.VeiculoRepresentationModel;
+import com.algaworks.algatransito.domain.model.Veiculo;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //Declara que essa classe é um componente do Spring com o objetivo de configuar Beans
+public class ModelMapperConfig {
+
+    @Bean //Essa anotação informa ao Spring que esse método deve ser usado para criar e gerenciar a instância do ModelMapper
+    public ModelMapper modelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+        /*
+        Nosso VeiculoRepresentationModel possui o atributo "numeroPlaca" que está sendo mapeado pelo Veiculo que possui o atributo "placa".
+        Na hora de criar a instância de Model Mapper está gerando um conflito, pois não temos "numero" em nenhum dos atributos.
+        O código abaixo está fazendo a configuração para resolver esse problema.
+         */
+        modelMapper.createTypeMap(Veiculo.class, VeiculoRepresentationModel.class)
+                .addMappings(mapper -> mapper.map(veiculo -> veiculo.getPlaca(),
+                        (VeiculoRepresentationModel veiculoRepresentationModel, String valor) -> veiculoRepresentationModel.setNumeroPlaca(valor)));
+
+        return modelMapper;
+    }
+
+}
